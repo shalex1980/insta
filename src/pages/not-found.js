472971d25871b9b3b03d3598e7f4cc95758f4405
Alex <alex@localhost.localdomain>
2020-12-12 20:37:24 +0200

    first init

---
+++ src/pages/not-found.js
@@ -0,0 +1,25 @@
+import { Typography } from "@material-ui/core"
+import React from "react"
+import Layout from "../components/shared/Layout"
+import { Link } from "react-router-dom"
+
+function NotFoundPage() {
+  return <Layout minimalNavbar title="Page Not Found">
+    <Typography variant="h5" align="center" pharagraph>
+      Sorry, this page isn`t available.
+    </Typography>
+    <Typography align="center">
+      The link you followed may be broken, or the page may have been removed.
+      <Link to="/">
+        {" "}
+        <Typography color="primary" component="span">
+          Go back to Instagram.
+        </Typography>
+      </Link>
+    </Typography>
+    
+    
+  </Layout>
+}
+
+export default NotFoundPage;
